5ea21cd0058e6c68140110ba92cca59b3d8a3aef
Adilwebpy <adiltazhibaev42@gmail.com>
2020-12-02 21:31:17 +0600

    Update

--- server/mysqlconnect.php
+++ server/mysqlconnect.php
@@ -1,17 +1,17 @@
 <?php
 
-	// $user = 'root';
-	// $password = 'root';
-	// $db = 'comments';
-  // $host = 'localhost';
-  $host = 'remotemysql.com';
-  $user = '1EPfYEIuvC';
-	$password = 'uDrSpceULg';
-	$db = '1EPfYEIuvC';
-  
-  $charset = 'utf8md4';
+// $user = 'root';
+// $password = 'root';
+// $db = 'comments';
+// $host = 'localhost';
+$user = 'root';
+$password = 'root';
+$db = 'comments';
+$host = 'localhost';
+$port = 3306;
+
+$charset = 'utf8md4';
 
 
-	$dsn = 'mysql:host='.$host.';dbname='.$db;
-	$pdo = new PDO($dsn, $user, $password);
-?>
+$dsn = 'mysql:host=' . $host . ';dbname=' . $db;
+$pdo = new PDO($dsn, $user, $password);
